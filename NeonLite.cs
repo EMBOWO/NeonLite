bd151828936feec1ecd03243acdaeb29081002a8
BuildTools <unconfigured@null.spigotmc.org>
2026-06-23 12:54:27 -0700

    fix

--- NeonLite.cs
+++ NeonLite.cs
@@ -82,7 +82,7 @@ namespace NeonLite
             const string URL = "https://raw.githubusercontent.com/Faustas156/NeonLite/main/Resources/locale.csv";
             const string URLEM = "https://raw.githubusercontent.com/EMBOWO/NeonLite/main/Resources/localeEM.csv";
             LC = Localization.GetLocale_Stream("NeonLite", Localization.Reader_CSVStream, Resources.locale.GetStream(), URL);
-            LCEM = Localization.GetLocale_Stream("NeonLite", Localization.Reader_CSVStream, Resources.locale.GetStream(), URL);
+            LCEM = Localization.GetLocale_Stream("NLEM", Localization.Reader_CSVStream, Resources.locale.GetStream(), URLEM);
         }
 
         internal static void ActivatePriority()
